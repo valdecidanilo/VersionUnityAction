67ae1965d9fc57ace60c807ee25652174bf3f579
Valdeci Danilo <valdeci.danilo@gdstecnologia.com.br>
2025-05-20 18:05:24 -0300

    fix: update info generator

--- Assets/BuildInfo/Editor/BuildInfoGenerator.cs
+++ Assets/BuildInfo/Editor/BuildInfoGenerator.cs
@@ -1,15 +1,27 @@
 using UnityEditor;
+using BuildInfo;
+using UnityEngine;
 
 namespace BuildInfo.Editor
 {
     public static class BuildInfoGenerator {
         [MenuItem("CI/Set Build Info")]
         public static void SetBuildInfo() {
-            var so = AssetDatabase.LoadAssetAtPath<BuildInfo>("Assets/BuildInfo/BuildInfo.asset");
-            so.versionSummary = System.Environment.GetEnvironmentVariable("FULL_VERSION_SUMMARY");
-            so.versionKey     = System.Environment.GetEnvironmentVariable("FULL_VERSION_KEY");
-            PlayerSettings.bundleVersion = so.versionSummary;
-            EditorUtility.SetDirty(so);
+            var summary = System.Environment.GetEnvironmentVariable("FULL_VERSION_SUMMARY");
+            var key     = System.Environment.GetEnvironmentVariable("FULL_VERSION_KEY");
+    
+            const string path = "Assets/BuildInfo/BuildInfo.asset";
+            var asset = AssetDatabase.LoadAssetAtPath<BuildInfo>(path);
+            if (asset == null)
+            {
+                asset = ScriptableObject.CreateInstance<BuildInfo>();
+                AssetDatabase.CreateAsset(asset, path);
+            }
+    
+            asset.versionSummary = summary ?? "";
+            asset.versionKey     = key     ?? "";
+    
+            EditorUtility.SetDirty(asset);
             AssetDatabase.SaveAssets();
         }
     }
