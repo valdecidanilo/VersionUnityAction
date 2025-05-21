f3b8ec8f7c71e7a6e73eb84e32248db5ed747733
Valdeci Danilo <valdeci.danilo@gdstecnologia.com.br>
2025-05-21 10:39:42 -0300

    fix: update infos version in txts

--- Assets/BuildInfo/BuildInfoUI.cs
+++ Assets/BuildInfo/BuildInfoUI.cs
@@ -1,16 +1,37 @@
+using System.Collections;
+using System.IO;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Networking;
 
 namespace BuildInfo
 {
     public class BuildInfoUI : MonoBehaviour
     {
-        [SerializeField] private TMPro.TMP_Text versionText;
-        [SerializeField] private BuildInfo buildInfo;
+        [SerializeField] private TMP_Text versionText;
 
-        private void Awake()
+        private IEnumerator Start()
         {
-            if (buildInfo && versionText)
-                versionText.text = buildInfo.versionKey;
+            var version = "n/a";
+            var path = Path.Combine(Application.streamingAssetsPath, "version.txt");
+
+            if (versionText == null)
+            {
+                Debug.LogWarning("[Build Info] VersionDisplay: versionText não atribuído");
+                yield break;
+            }
+
+            if (Application.platform == RuntimePlatform.WebGLPlayer)
+            {
+                using var www = UnityWebRequest.Get(path);
+                yield return www.SendWebRequest();
+                if (www.result == UnityWebRequest.Result.Success)
+                    version = www.downloadHandler.text.Trim();
+                else
+                    Debug.LogError($"[Build Info] Ao ler version.txt: {www.error}");
+            }
+
+            versionText.text = version;
         }
     }
 }
